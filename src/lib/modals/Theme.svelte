359342d7cf1b088dad418e9fa945f91350830344
M.D. Walters <808426@pdsb.net>
2022-11-04 13:04:59 -0400

    AAAAAAAA I wish I still had my huge laptop that had early BetterMeower 1.4.0 on it :cry:

--- src/lib/modals/Theme.svelte
+++ src/lib/modals/Theme.svelte
@@ -7,56 +7,62 @@
 <Modal on:close={() => {$modalShown = false}}>
     <h2 slot="header">Set Theme</h2>
     <div slot="default">
-		<div class="modal-buttons">
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "orange";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Orange</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "blue";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Blue</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "green";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Green</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "magenta";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Magenta</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "red";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Red</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "pink";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Pink</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "gray";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Gray</button>
-            <button on:click={() => {
-                const _user = $user;
-				_user.theme = "purple";
-				user.set(_user);
-				clm.updateProfile();
-            }}>Purple</button>
-            <button on:click={() => {$modalShown = false}}>Close</button>
-        </div>
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "orange";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Orange</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "blue";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Blue</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "green";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Green</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "magenta";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Magenta</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "red";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Red</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "pink";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Pink</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "gray";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Gray</button>
+
+        <button on:click={() => {
+            const _user = $user;
+			_user.theme = "purple";
+			user.set(_user);
+			clm.updateProfile();
+        }}>Purple</button>
+
+        <button on:click={() => {$modalShown = false}}>Close</button>
 	</div>
 </Modal>
