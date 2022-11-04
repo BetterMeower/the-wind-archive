1b254d8a1bf0d9c29390f86a51478be95193b742
M.D. Walters <808426@pdsb.net>
2022-11-04 13:41:47 -0400

    PLEASE WORK PLEASE WORK

--- src/lib/modals/Theme.svelte
+++ src/lib/modals/Theme.svelte
@@ -4,9 +4,9 @@
 	import {modalShown, user} from "../stores.js";
 </script>
 
-<Modal on:close={() => {$modalShown = false}} class="display: grid; gap: 5px">
+<Modal on:close={() => {$modalShown = false}}>
     <h2 slot="header">Set Theme</h2>
-    <div slot="default">
+    <div slot="default" class="grid">
         <button on:click={() => {
             const _user = $user;
 			_user.theme = "orange";
@@ -66,3 +66,12 @@
         <button on:click={() => {$modalShown = false}}>Close</button>
 	</div>
 </Modal>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: auto auto auto auto;
+        grid-column-gap: 5px;
+        grid-row-gap: 5px;
+    }
+</style>
